4c4301bb333339d1ed13fd7771e40d53487a31f5
Pooja Mahesh <pooja11mahesh@gmail.com>
2014-10-22 19:09:53 -0400

    10-15-2014-PoojaMahesh-RemoteImage
    Tried the code done in class on Wednesday

---
+++ app/src/main/java/net/remoteimage/pooja/mysampleremoteimage/MyRemoteImageActivity.java
@@ -0,0 +1,80 @@
+package net.remoteimage.pooja.mysampleremoteimage;
+
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class MyRemoteImageActivity extends Activity {
+
+
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_my_remote_image);
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my_remote_image, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_my_remote_image, container, false);
+
+
+            RemoteImage ri = new RemoteImage(getActivity());
+            ri.execute("http://wanderingoak.net/bridge.png");
+
+            return rootView;
+        }
+    }
+
+
+}
